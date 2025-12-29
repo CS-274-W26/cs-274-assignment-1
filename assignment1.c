49b6c131cc4d237be06556c4b34e4f601aadf6a2
Alexander Guyer <guyera@oregonstate.edu>
2025-12-29 14:18:06 -0800

    Starter code

---
+++ assignment1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Necessary for printf()
+
+// TODO Other includes
+
+
+int main() {
+	// The main function is the entrypoint of the program. When the program
+	// is executed, it will automatically start here.
+	
+	// TODO Complete the program
+}
